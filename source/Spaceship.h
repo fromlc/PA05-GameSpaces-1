cf7839e5e5cdd7dda1e5bf487d150883d107b617
L Carver <wifihum@gmail.com>
2026-03-14 12:40:55 -0700

    Added Spaceship class files with declaration but empty definition.

---
+++ source/Spaceship.h
@@ -0,0 +1,31 @@
+//------------------------------------------------------------------------------
+// Spaceship.h : derived class declaration, base class GameSpace
+//------------------------------------------------------------------------------
+#pragma once
+
+#include "GameSpace.hpp"
+
+class Spaceship : public GameSpace
+{
+private:
+    int speed;
+    bool inOrbit;
+    bool onTheWay;
+
+public:
+    Spaceship();
+
+    int getSpeed() const;
+    void setSpeed(int spd);
+
+    bool isInOrbit() const;
+    void setInOrbit(bool orbiting);
+
+    bool isOnTheWay() const;
+    void setOnTheWay(bool going);
+
+    void listOccupants();
+
+    void print();
+};
+
