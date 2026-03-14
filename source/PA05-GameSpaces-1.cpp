27250745865b0ae849c20f3cb36a21ded7991b13
L Carver <wifihum@gmail.com>
2026-03-14 12:04:24 -0700

    Move addEntity() calls from PlanetX constructor to app code. Display initial planet status.

--- source/PA05-GameSpaces-1.cpp
+++ source/PA05-GameSpaces-1.cpp
@@ -8,5 +8,25 @@
 int main()
 {
     PlanetX px;
+    px.setName("Planet X");
+    px.setDescription(
+        " is a giant planet with methane oceans and enormous active volcanoes.");
+
+	// BEGIN Don't change these!
+	// mech Entity, don't rescue
+	px.addEntity({ "Rover 1 explorer drone", -10 });
+	// living bio Entity, do rescue
+	px.addEntity({ "Mysterious vapor being", 10 });
+	// non-living bio, don't rescue
+	px.addEntity({ "Aquarium fish", 0 });
+	// END Don't change these!
+
+	// add your own occupants here
+
+	// mech Entities have a negative health value.
+	// non-living biological Entities have a health value of 0.
+	// living biological Entities have a positive health value.
+
+	// Initial planet status
     px.print();
 }
--- source/PlanetX.cpp
+++ source/PlanetX.cpp
@@ -8,20 +8,6 @@
 // constructor
 PlanetX::PlanetX()
 {
-	name = "Planet X";
-	description = "a giant planet with methane oceans and giant volcanoes";
-
-	// BEGIN Don't change these!
-	addEntity({ "Rover 1 explorer drone", -10 });	// mech Entity, don't rescue
-	addEntity({ "Mysterious vapor being", 10 });	// living bio, do rescue
-	addEntity({ "Aquarium fish", 0 });				// non-living bio, don't rescue
-	// END Don't change these!
-
-	// add your own occupants here
-
-	// mech Entities have a negative health value.
-	// non-living biological Entities have a health value of 0.
-	// living biological Entities have a positive health value.
 }
 
 // display status and occupant list 
@@ -30,17 +16,17 @@ void PlanetX::print()
 	std::cout << name << description << "\n";
 
 	size_t eCount = getEntityCount();
-
 	std::cout << name << " has " << eCount << " occupants:\n";
 
-	for (int i = 0; i < eCount; i++)
+	for (size_t i = 0; i < eCount; i++)
 	{
 		Entity ent = getEntity(i);
-		std::cout << "\t" << ent.name;
+		std::cout << "\t" << ent.getName();
 		
-		if (ent.health > 0)
+		int eHealth = ent.getHealth();
+		if (eHealth > 0)
 			std::cout << " is alive!\n";
-		else if (ent.health == 0)
+		else if (eHealth == 0)
 			std::cout << " is unfortunately dead :(\n";
 		else
 			std::cout << " is a mech\n";
